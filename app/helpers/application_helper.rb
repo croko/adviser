7f965a9eb862da4aa18d417d837807e9de966711
Gennady Kudelya <gennady.kudelya@gmail.com>
2013-07-15 16:12:35 +0300

    Initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,17 @@
+module ApplicationHelper
+
+  def rating_box rating = 0.0
+    stars = []
+    (1..5).each do |i|
+      if i <= rating + 0.335
+        stars << image_tag('star_activ.png')
+      elsif i - rating > 0.335 and i - rating < 0.665
+        stars << image_tag('half_star_activ.png')
+      else
+        stars << image_tag('star_inactiv.png')
+      end
+    end
+    raw stars.join
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adviser::Application.config.secret_key_base = '7f5c020339578e85f1447292dbe7939066f97ba721848ec9a24b0845714b82c9fa222f9e6765a94d00a592f5898638215c2e0a1a49de4a5ce0fb680f6a2f53bd'
